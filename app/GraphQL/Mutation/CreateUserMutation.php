6cff12132fb427a017dc892af61446934c7e699d
xingobar <gary840227@gmail.com>
2020-09-21 22:44:35 +0800

    create user validation

--- app/GraphQL/Mutation/CreateUserMutation.php
+++ app/GraphQL/Mutation/CreateUserMutation.php
@@ -7,6 +7,7 @@ namespace App\GraphQL\Mutation;
 use App\User;
 use GraphQL\Type\Definition\Type as GraphqlType;
 use GraphQL\Type\Definition\Type;
+use Illuminate\Contracts\Validation\Validator as ValidatorContract;
 use Illuminate\Support\Facades\Hash;
 use Rebing\GraphQL\Support\Facades\GraphQL;
 use Rebing\GraphQL\Support\Mutation;
@@ -20,6 +21,37 @@ class CreateUserMutation extends Mutation
     public function type(): GraphqlType
     {
        return GraphQL::type('users');
+    }
+
+    public function rules(array $args = []): array
+    {
+        return [
+          'name' => [
+              'required',
+              'max:10'
+          ],
+          'email' => [
+              'required',
+              'email',
+              'unique:users'
+          ],
+          'password' => [
+              'required',
+              'min:6'
+          ]
+        ];
+    }
+
+    public function validationErrorMessages(array $args = []): array
+    {
+        return [
+            'name.required' => '請輸入名稱',
+            'name.max' => '名字最長 :max 字',
+            'email.email' => '電子郵件格式不符',
+            'email.unique' => '電子郵件重複',
+            'password.required' => '請輸入密碼',
+            'password.min' => '密碼至少 :min 長度'
+        ];
     }
 
     public function args(): array
